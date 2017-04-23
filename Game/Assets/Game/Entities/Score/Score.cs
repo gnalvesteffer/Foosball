d90bdb3aad943e25d680944fea5ead0d6749a7a3
ss-gnalvesteffer <gavin.alvesteffer@shipstation.com>
2017-04-22 19:33:05 -0500

    Initial Commit.

---
+++ Game/Assets/Game/Entities/Score/Score.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public Sprite[] ScoreSprites;
+
+    private void Start ()
+    {
+		
+	}
+
+    private void Update ()
+    {
+		
+	}
+}
